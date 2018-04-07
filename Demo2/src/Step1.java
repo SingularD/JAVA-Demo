34cb2da356abded829e6ac446287ca5471c34e0e
SingularD <906229740@qq.com>
2018-04-07 21:47:01 +0800

    Java homework

--- Demo2/src/Step1.java
+++ Demo2/src/Step1.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Step1 {
     public static void main(String []args){
-        Scanner s = new Scanner(System.in);
-        System.out.println("请输入：");
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("请输入：");
 //        if(s.hasNext()){
 //            String str1 = s.next();
 //            System.out.println(str1);
 //        }
-        if(s.hasNextLine()){
-            String str2 = s.nextLine();
-            System.out.println(str2);
-        }
+//        if(s.hasNextLine()){
+//            String str2 = s.nextLine();
+//            System.out.println(str2);
+//        }
+//        System.out.println("I'm lisongwei");
+//        System.out.println("I'm a handsome boy");
+//        System.out.print("I'm lisongwei\n");
+//        System.out.print("I'm a handsome boy");
     }
 }
---
+++ Java Homework/第二章作业+李松蔚+2016100104019/1/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args){
+//        int i = j = k = 2;
+//        System.out.println(i+ " " + j + " " + k);
+//        错误：定义int类型方法错误！
+//        正确方法如下：
+
+//      int i = 2,j = 2,k = 2;或者：
+        int i,j,k;
+        i=j=k=2;
+        System.out.println(i + " " + j + " " + k );
+    }
+}
