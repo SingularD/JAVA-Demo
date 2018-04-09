69238434d426c921d5a09e133804d3d124404a23
SingularD <906229740@qq.com>
2018-04-09 16:38:49 +0800

    homework

---
+++ Java Homework/第三章作业+李松蔚+2016100104019/3.9/src/Test.java
@@ -0,0 +1,33 @@
+public class Test {
+    public static void main(String[] args){
+//        int x =2 ,y = 3;
+//        if(x > 2){
+//            if(y > 2){
+//                int z = x + y;
+//                System.out.println("z is :" + z);
+//            }
+//        }else {
+//            System.out.println("x is :" + x);
+//        } output is : "x is : 2"
+
+//        int x = 3 , y =2;
+//        if(x > 2)
+//            if(y > 2){
+//                int z = x + y;
+//                System.out.println("z is :" + z);
+//            }
+//        else
+//            System.out.println("x is :" + x);
+//            output is : "x is : 3"
+
+        int x = 3 , y =3;
+        if(x > 2)
+            if(y > 2){
+                int z = x + y;
+                System.out.println("z is :" + z);
+            }
+            else
+                System.out.println("x is :" + x);
+//        output is : "z is : 6"
+    }
+}
