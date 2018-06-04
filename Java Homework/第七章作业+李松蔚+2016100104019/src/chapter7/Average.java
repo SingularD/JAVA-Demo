295744cb1fa76e028469e06b6f3359073c3ef590
SingularD <906229740@qq.com>
2018-06-04 23:16:37 +0800

    java homework

--- Java Homework/第七章作业+李松蔚+2016100104019/src/chapter7/Average.java
+++ Java Homework/第七章作业+李松蔚+2016100104019/src/chapter7/Average.java
@@ -1,4 +1,19 @@
 package chapter7;
 
 public class Average {
+    public static int average(int[] array){
+        int sum = 0;
+        for (int i = 0;i < array.length;i++){
+            sum += array[i];
+        }
+        return sum/array.length;
+    }
+    public static double average(double[] array){
+        int sum = 0;
+        for (int i = 0;i < array.length;i++){
+            sum += array[i];
+        }
+        return sum/array.length;
+    }
+    //
 }
