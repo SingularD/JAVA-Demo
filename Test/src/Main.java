232fc514d7dc4503314591a1216115a0c5947a4e
SingularD <906229740@qq.com>
2018-06-06 21:37:22 +0800

    javaFX

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,20 +1,5 @@
+import javafx.application.Application;
 
+public class MyJavaFX extends Application{
 
-import uestc.People;
-
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        People student1 = new People("lisongwei");
-        People student2 = new People("liangtao");
-        student1.peoAge(12);
-        student2.peoAge(15);
-        student1.peoId(12308);
-        student2.peoId(91241);
-        student1.peoGrade("one");
-        student2.peoGrade("two");
-        student1.printF();
-        student2.printF();
-    }
 }
